bc75df9f5bc4fd7cb49041f8b6f4a5a11ae7cab1
linonetwo <linonetwo012@gmail.com>
2020-01-24 17:45:04 +0800

    lint: prettier

--- webpack.development.js
+++ webpack.development.js
@@ -1,35 +1,35 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
-const merge = require("webpack-merge");
-const path = require("path");
-const base = require("./webpack.config");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
+const merge = require('webpack-merge');
+const path = require('path');
+const base = require('./webpack.config');
 
 module.exports = merge(base, {
-  mode: "development",
-  devtool: "source-map", // Show the source map so we can debug when developing locally
+  mode: 'development',
+  devtool: 'source-map', // Show the source map so we can debug when developing locally
   devServer: {
-    host: "localhost",
-    port: "40992",
+    host: 'localhost',
+    port: '40992',
     hot: true, // Hot-reload this server if changes are detected
     compress: true, // Compress (gzip) files that are served
-    contentBase: path.resolve(__dirname, "app/dist"), // Where we serve the local dev server's files from
+    contentBase: path.resolve(__dirname, 'app/dist'), // Where we serve the local dev server's files from
     watchContentBase: true, // Watch the content base for changes
     watchOptions: {
-      ignored: /node_modules/, // Ignore this path, probably not needed since we define contentBase above
-    },
+      ignored: /node_modules/ // Ignore this path, probably not needed since we define contentBase above
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "app/src/index.html"),
-      filename: "index.html",
+      template: path.resolve(__dirname, 'app/src/index.html'),
+      filename: 'index.html'
     }),
     new CspHtmlWebpackPlugin({
-      "base-uri": ["'self'"],
-      "object-src": ["'none'"],
-      "script-src": ["'self'"],
-      "style-src": ["'self'"],
-      "frame-src": ["'none'"],
-      "worker-src": ["'none'"],
-    }),
-  ],
-})
+      'base-uri': ["'self'"],
+      'object-src': ["'none'"],
+      'script-src': ["'self'"],
+      'style-src': ["'self'"],
+      'frame-src': ["'none'"],
+      'worker-src': ["'none'"]
+    })
+  ]
+});
--- webpack.production.js
+++ webpack.production.js
@@ -1,24 +1,24 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
-const merge = require("webpack-merge");
-const path = require("path");
-const base = require("./webpack.config");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
+const merge = require('webpack-merge');
+const path = require('path');
+const base = require('./webpack.config');
 
 module.exports = merge(base, {
-  mode: "production",
-  devtool: "nosources-source-map", // https://webpack.js.org/configuration/devtool/ && https://github.com/webpack/webpack/issues/5627#issuecomment-389492939
+  mode: 'production',
+  devtool: 'nosources-source-map', // https://webpack.js.org/configuration/devtool/ && https://github.com/webpack/webpack/issues/5627#issuecomment-389492939
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "app/src/index-prod.html"),
-      filename: "index-prod.html",
+      template: path.resolve(__dirname, 'app/src/index-prod.html'),
+      filename: 'index-prod.html'
     }),
     new CspHtmlWebpackPlugin({
-      "base-uri": ["'self'"],
-      "object-src": ["'none'"],
-      "script-src": ["'self'"],
-      "style-src": ["'self'"],
-      "frame-src": ["'none'"],
-      "worker-src": ["'none'"],
-    }),
-  ],
+      'base-uri': ["'self'"],
+      'object-src': ["'none'"],
+      'script-src': ["'self'"],
+      'style-src': ["'self'"],
+      'frame-src': ["'none'"],
+      'worker-src': ["'none'"]
+    })
+  ]
 });
