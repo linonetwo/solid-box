19ecb2d28d4d8abffe0da9f3fbe0fd2f570876c2
linonetwo <linonetwo012@gmail.com>
2020-01-28 14:35:06 +0800

    fix: handle where win is destroyed

--- app/electron/main.js
+++ app/electron/main.js
@@ -136,12 +136,13 @@ app.on('window-all-closed', () => {
   // if (process.platform !== 'darwin') {
   //   app.quit();
   // }
+  win = null;
 });
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+  if (typeof win === 'undefined' || win === null || win.isDestroyed()) {
     createWindow();
   }
 });
