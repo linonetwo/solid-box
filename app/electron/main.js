b5cc4c40f83a8e2b821ab6c95cc1ee0f867c788f
linonetwo <linonetwo012@gmail.com>
2020-01-30 19:40:21 +0800

    lint

--- app/electron/main.js
+++ app/electron/main.js
@@ -130,10 +130,10 @@ protocol.registerSchemesAsPrivileged([
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
+  if (process.platform !== 'darwin') {
     app.quit();
   } else {
     i18nextBackend.clearMainBindings(ipcMain); // added!
